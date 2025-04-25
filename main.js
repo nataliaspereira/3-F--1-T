369395357cd60642f670156f7916db6d4f46d43d
nataliaspereira <160850395+nataliaspereira@users.noreply.github.com>
2025-04-25 10:53:55 -0300

    Natália e Vivian

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
- let 
+ let randomNumber = 
+  Math.floor(Math.random() *10 ) + 1;
+  let attemps = 0;
+
+  function checsGuess () {
+    const userGuess = parseInt (document.getElementById ('guess').ariaValueMax)
+    attemps
+  }
